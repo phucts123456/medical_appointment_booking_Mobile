b1e7575397e3be1622a7b5de64a67c039da93ddd
QuangBG <qbuigia@yahoo.com.vn>
2022-03-16 13:56:04 +0700

    Add hash password when create doctor

--- app/src/main/java/app/folder/medical_appointment_booking/AdminCreateDoctorActivity.java
+++ app/src/main/java/app/folder/medical_appointment_booking/AdminCreateDoctorActivity.java
@@ -29,6 +29,9 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -126,10 +129,32 @@ public class AdminCreateDoctorActivity extends AppCompatActivity {
         });
     }
 
+    private static String bytesToHex(byte[] hash) {
+        StringBuilder hexString = new StringBuilder(2 * hash.length);
+        for (int i = 0; i < hash.length; i++) {
+            String hex = Integer.toHexString(0xff & hash[i]);
+            if(hex.length() == 1) {
+                hexString.append('0');
+            }
+            hexString.append(hex);
+        }
+        return hexString.toString();
+    }
+
     public void SaveDoctor(View view) {
         queue = Volley.newRequestQueue(this);
         String url ="https://medical-appointment-booking.herokuapp.com/api/Doctors/Admin";
         JSONObject object = new JSONObject();
+        MessageDigest digest = null;
+        try {
+            digest = MessageDigest.getInstance("SHA-256");
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        String password = edtPassword.getText().toString();
+        byte[] encodedhash = digest.digest(
+                password.getBytes(StandardCharsets.UTF_8));
+        String hashPassword = bytesToHex(encodedhash);
         try{
             try {
                 object.put("specialistId", selectedId);//edtSpecialistId.getText().toString());
@@ -141,7 +166,7 @@ public class AdminCreateDoctorActivity extends AppCompatActivity {
             object.put("academicRank", edtAcademicRank.getText());
             object.put("isMale", chkMale.isChecked());
             object.put("username", edtUsername.getText());
-            object.put("password", edtPassword.getText());
+            object.put("password", hashPassword);
 
         } catch (JSONException e){
             e.printStackTrace();
